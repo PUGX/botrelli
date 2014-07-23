cfbc1d2fade3a2afb4c472477a1c9f1ff5eb78c5
Francesco Face <f.face@noah.it>
2014-07-23 22:17:12 +0200

    Added more than one funny message

--- src/Infrastructure/FunnyMessageRepository.php
+++ src/Infrastructure/FunnyMessageRepository.php
@@ -7,6 +7,19 @@ use \PUGX\Bot\Message\MessageRepositoryInterface;
 
 class FunnyMessageRepository implements MessageRepositoryInterface
 {
+
+    private $messages = array();
+
+    public function __construct()
+    {
+        $messages[] = "D'oh!";
+        $messages[] = "This is nearly perfect, now!";
+        $messages[] = "Spring cleaning...";
+        $messages[] = "Details make perfection, and perfection is not a detail (Leonardo Da Vinci)";
+        $messages[] = "No thanks needed, dude!";
+        $this->messages = $messages;
+    }
+
     /**
      * @return Message
      *
@@ -14,6 +27,8 @@ class FunnyMessageRepository implements MessageRepositoryInterface
      */
     public function fetch()
     {
-       throw new \Exception('Missing funny message');
+       $i = rand(0, sizeof($this->messages)-1);
+       return $this->messages[$i];
     }
-} 
+
+}
