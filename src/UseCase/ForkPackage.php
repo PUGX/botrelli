c42a54f35e9d4597c2c9cca1169156631f039c87
Andrea Giannantonio <jellybelly@new-host.station>
2014-07-23 21:29:58 +0200

    repaired Package

--- src/UseCase/ForkPackage.php
+++ src/UseCase/ForkPackage.php
@@ -6,8 +6,16 @@ use Packagist\Api\Result\Package;
 
 class ForkPackage
 {
+    private $client;
+
+    public function  __construct($client)
+    {
+        $this->client = $client;
+    }
+
     public function execute(Package $package)
     {
+        $repository = $this->client->api('repo')->forks()->create($package->getName(), $package->get);
         return false;
     }
 } 
