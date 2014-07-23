0b482cc037c09b6518f4b4455cb92cfbffc6f35c
Marco Loche <marco.loche@capturator.com>
2014-07-23 22:47:42 +0200

    UseCase n.6 "MakeAPR"

--- src/Tests/UseCase/MakeAPRTest.php
+++ src/Tests/UseCase/MakeAPRTest.php
@@ -2,7 +2,7 @@
 
 namespace PUGX\Bot\Tests\Package;
 
-use Packagist\Api\Result\Package;
+use PUGX\Bot\Package;
 use PUGX\Bot\LocalPackage;
 use PUGX\Bot\UseCase\MakeAPR;
 
@@ -13,13 +13,61 @@ class MakeAPRTest extends \PHPUnit_Framework_TestCase
      */
     public function shouldBeAbleToMakeAPR()
     {
-        $this->markTestIncomplete();
+        $package = $this->getMock('\PUGX\Bot\Package');
+        $package
+            ->expects($this->once())
+            ->method('getUsername')
+            ->will($this->returnValue('pugx'));
+        $package
+            ->expects($this->once())
+            ->method('getRepoName')
+            ->will($this->returnValue('botrelli'));
+        $localPackage = new LocalPackage('/tmp', $package);
 
         $messageRepository = $this->getMock('\PUGX\Bot\Message\MessageRepositoryInterface');
+        $messageRepository
+            ->expects($this->once())
+            ->method('fetch')
+            ->will($this->returnValue('A funny message'));
 
-        $package = new LocalPackage('/tmp', new Package());
+        $pullRequest = $this->getMockBuilder('\Github\Api\PullRequest')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $pullRequest
+            ->expects($this->once())
+            ->method('create')
+            ->with($this->equalTo('pugx'),
+                $this->equalTo('botrelli'),
+                $this->equalTo(array('base' => 'master', 'head' => 'master', 'title' => 'CS Fixes', 'body' => 'A funny message')));
 
-        $command = new MakeAPR($messageRepository);
-        $this->assertTrue($command->execute($package));
+        $response = $this->getMockBuilder('\Guzzle\Http\Message\Response')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $response
+            ->expects($this->once())
+            ->method('getStatusCode')
+            ->will($this->returnValue(201));
+
+        $httpClient = $this->getMock('\Github\HttpClient\HttpClient');
+        $httpClient
+            ->expects($this->once())
+            ->method('getLastResponse')
+            ->will($this->returnValue($response));
+
+        $client = $this->getMock('\Github\Client');
+        $client
+            ->expects($this->once())
+            ->method('api')
+            ->with($this->equalTo('pull_request'))
+            ->will($this->returnValue($pullRequest));
+
+        $client
+            ->expects($this->once())
+            ->method('getHttpClient')
+            ->will($this->returnValue($httpClient));
+
+        $command = new MakeAPR($client, $messageRepository);
+
+        $this->assertTrue($command->execute($localPackage));
     }
 }
--- src/UseCase/MakeAPR.php
+++ src/UseCase/MakeAPR.php
@@ -4,20 +4,30 @@ namespace PUGX\Bot\UseCase;
 
 use PUGX\Bot\LocalPackage;
 use PUGX\Bot\Message\MessageRepositoryInterface;
+use Github\Client;
 
 class MakeAPR
 {
     private $messageRepository;
+    private $client;
 
-    function __construct(MessageRepositoryInterface $messageRepository)
+    function __construct(Client $client, MessageRepositoryInterface $messageRepository)
     {
         $this->messageRepository = $messageRepository;
+        $this->client = $client;
     }
 
     public function execute(LocalPackage $package)
     {
         $message = (string)$this->messageRepository->fetch();
-
-        return false;
+        $pullRequest = $this->client
+            ->api('pull_request')
+            ->create( $package->getUsername(), $package->getRepoName(), array(
+                'base' => 'master',
+                'head' => 'master',
+                'title' => 'CS Fixes',
+                'body' => $message
+            ));
+        return 201 === $this->client->getHttpClient()->getLastResponse()->getStatusCode();
     }
 } 
